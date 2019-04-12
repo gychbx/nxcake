7c306b9fcf1a3cffbefe0576bdb827299df53dfc
gyc <844937952@qq.com>
2019-04-12 09:16:31 +0800

    999

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <my-header></my-header>
     <router-view/>
     <my-foot></my-foot>
   </div>
 </template>
 
 <script>
+import MyHeader from "./components/MyHeader"
 import MyFoot from "./components/MyFoot"
 export default {
   name: 'App',
   components:{
-  	"my-foot":MyFoot
+    "my-foot":MyFoot,
+    "my-header":MyHeader
   }
 }
 </script>
